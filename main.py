24b69d9e98e501a1f79058070019cc2c2e64f7c3
jcaole <jeremy_1072@yahoo.com>
2023-02-09 14:15:07 -0600

    changed title

--- main.py
+++ main.py
@@ -46,7 +46,7 @@ def main() -> None:
         screen_width,
         screen_height,
         tileset=tileset,
-        title="Yet Another Roguelike Tutorial",
+        title="CamerataUntitiledGame",
         vsync=True,
     ) as context:
         root_console = tcod.Console(screen_width, screen_height, order="F")
