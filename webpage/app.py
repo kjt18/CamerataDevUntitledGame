ad3232cd5eee22a72c50970e1f6b48b091412e88
jcaole <jeremy_1072@yahoo.com>
2023-03-08 17:21:06 -0600

    use command 'flask run' to test web app

---
+++ webpage/app.py
@@ -0,0 +1,22 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+
+@app.route('/game')
+def game():
+    return render_template('game.html')
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
