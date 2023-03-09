f4edee3824c1bf866d46601f2087fcdb74365b94
jcaole <jeremy_1072@yahoo.com>
2023-03-08 21:48:01 -0600

    added carousel with placeholder images

--- webpage/app.py
+++ webpage/app.py
@@ -1,11 +1,23 @@
 from flask import Flask, render_template
+# import os
+
+# DOG_FOLDER = os.path.join('static', 'dog_photo')
 
 app = Flask(__name__)
+# app.config['UPLOAD_FOLDER'] = DOG_FOLDER
 
 
 @app.route('/')
+@app.route('/index')
 def index():
     return render_template('index.html')
+
+
+# @app.route('/')
+# @app.route('/index')
+# def show_index():
+#     full_filename = os.path.join(app.config['UPLOAD_FOLDER'], 'dog.png', 'dog2.png', 'dog3.png')
+#     return render_template("index.html", user_image=full_filename)
 
 
 @app.route('/about')
