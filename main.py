21ed53fad3d417939feb935cf411e01e19e160d0
kjt18 <kjt18@students.uwf.edu>
2023-03-08 22:07:45 -0600

    Working on event handler

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
 #!/usr/bin/env python3
 import copy
+import random
+import sys
 import traceback
-
+import zerorpc
 import tcod
+from pynput.keyboard import Key, Controller
 
 import color
 from engine import Engine
@@ -13,67 +16,86 @@ from procgen import generate_dungeon
 import setup_game
 
 
-def main() -> None:
-    screen_width = 80
-    screen_height = 50
+class Main:
 
-    map_width = 80
-    map_height = 43
+    def __init__(self):
+        # random.seed(sys.argv[1])
+        self.keyboard = Controller()
+        self.main()
 
-    room_max_size = 10
-    room_min_size = 6
-    max_rooms = 30
+    def main(self) -> None:
 
-    max_monsters_per_room = 2
-    max_items_per_room = 2
+        self.screen_width = 80
+        self.screen_height = 50
 
-    tileset = tcod.tileset.load_tilesheet(
-        "dejavu10x10_gs_tc.png", 32, 8, tcod.tileset.CHARMAP_TCOD
-    )
+        map_width = 80
+        map_height = 43
 
-    player = copy.deepcopy(entity_factories.player)
+        room_max_size = 10
+        room_min_size = 6
+        max_rooms = 30
 
-    engine = setup_game.new_game()
+        max_monsters_per_room = 2
+        max_items_per_room = 2
 
-    engine.game_map = generate_dungeon(
-        max_rooms=max_rooms,
-        room_min_size=room_min_size,
-        room_max_size=room_max_size,
-        map_width=map_width,
-        map_height=map_height,
-        # max_monsters_per_room=max_monsters_per_room,
-        # max_items_per_room=max_items_per_room,
-        engine=engine,
-    )
-    engine.update_fov()
+        self.tileset = tcod.tileset.load_tilesheet(
+            "dejavu10x10_gs_tc.png", 32, 8, tcod.tileset.CHARMAP_TCOD
+        )
 
-    handler: input_handlers.BaseEventHandler = input_handlers.MainGameEventHandler(engine)
+        player = copy.deepcopy(entity_factories.player)
 
-    with tcod.context.new_terminal(
-            screen_width,
-            screen_height,
-            tileset=tileset,
-            title="CamerataDevUntitledGame",
-            vsync=True,
-    ) as context:
-        root_console = tcod.Console(screen_width, screen_height, order="F")
-        while True:
-            root_console.clear()
-            handler.on_render(console=root_console)
-            context.present(root_console)
+        engine = setup_game.new_game()
 
-            try:
-                for event in tcod.event.wait():
-                    context.convert_event(event)
-                    handler = handler.handle_events(event)
-            except Exception:  # Handle exceptions in game.
-                traceback.print_exc()  # Print error to stderr.
-                # Then print the error to the message log.
-                if isinstance(handler, input_handlers.EventHandler):
-                    handler.engine.message_log.add_message(
-                        traceback.format_exc(), color.error
-                    )
+        engine.game_map = generate_dungeon(
+            max_rooms=max_rooms,
+            room_min_size=room_min_size,
+            room_max_size=room_max_size,
+            map_width=map_width,
+            map_height=map_height,
+            # max_monsters_per_room=max_monsters_per_room,
+            # max_items_per_room=max_items_per_room,
+            engine=engine,
+        )
+        engine.update_fov()
+
+        self.handler: input_handlers.BaseEventHandler = input_handlers.MainGameEventHandler(engine)
+
+        self.root_console = tcod.Console(self.screen_width, self.screen_height, order="F")
+
+        self.context = tcod.context.new_terminal(
+                self.screen_width,
+                self.screen_height,
+                tileset=self.tileset,
+                title="CamerataDevUntitledGame",
+                vsync=True,)
+        self.render_console()
+
+    def move_up(self):
+
+        self.handle_event(tcod.event.K_UP)
+        self.render_console()
+
+        return self.root_console.__str__()
+
+    def handle_event(self, event):
+        try:
+            self.context.convert_event(event)
+            self.handler = self.handler.handle_events(event)
+        except Exception:  # Handle exceptions in game.
+            traceback.print_exc()  # Print error to stderr.
+            # Then print the error to the message log.
+            if isinstance(self.handler, input_handlers.EventHandler):
+                self.handler.engine.message_log.add_message(
+                    traceback.format_exc(), color.error
+                )
+
+    def render_console(self):
+        self.root_console.clear()
+        self.handler.on_render(console=self.root_console)
+        self.context.present(self.root_console)
 
 
 if __name__ == "__main__":
-    main()
+    s = zerorpc.Server(Main())
+    s.bind("tcp://0.0.0.0:4242")
+    s.run()
---
+++ test_rpc.py
@@ -0,0 +1,6 @@
+
+import zerorpc
+
+c = zerorpc.Client()
+c.connect("tcp://127.0.0.1:4242")
+print(c.move_up())
