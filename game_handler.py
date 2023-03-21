41c3b23fe75f83948dd1c6b6d3ca089bb8939f78
kjt18 <kjt18@students.uwf.edu>
2023-03-21 18:00:42 -0500

    game_handler.py now supports multiple instances running at once.

--- game_handler.py
+++ game_handler.py
@@ -3,14 +3,37 @@ import multiprocessing
 from main import start_instance
 
 
-def main():
+class Instance:
 
-    parent_conn, child_conn = multiprocessing.Pipe(duplex=True)
-    p = multiprocessing.Process(target=start_instance, args=(child_conn,))
-    p.start()
+    def __init__(self, name):
+        self.name = name
+        parent_conn, child_conn = multiprocessing.Pipe(duplex=True)
+        self.pipe = parent_conn
+        p = multiprocessing.Process(target=start_instance, args=(child_conn,))
+        p.start()
+
+    def command(self, command) -> str:
+        self.pipe.send(command)
+        return self.pipe.recv()
+
+
+# todo change this to communicate with client through socket instead of using input()
+# todo add support for paired instances that will share a seed
+def main():
+    instances = []
     while True:
-        parent_conn.send(str(input()))
-        print(parent_conn.recv())
+        found = False
+        query = str(input())
+        print(query)
+        name, command = query.split(',')
+        for x in instances:
+            if x.name == name:
+                print(x.command(command))
+                found = True
+        if not found:
+            new_instance = Instance(name)
+            print(new_instance.command(command))
+            instances.append(new_instance)
 
 
 if __name__ == "__main__":
