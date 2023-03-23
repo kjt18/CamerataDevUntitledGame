adb256eb77cfc6ced27c0bab926c096c7254aff4
Jacob Borelli <jwb50@students.uwf.edu>
2023-03-23 18:33:31 -0500

    ignore

--- webpage/app.py
+++ webpage/app.py
@@ -13,7 +13,7 @@ socketio = SocketIO(app)
 app.config['MYSQL_USER'] = 'capstonesa'
 app.config['MYSQL_PASSWORD'] = 'C@pstonepassword'
 app.config['MYSQL_DB'] = 'capstone'
-app.config['MYSQL_HOST'] = 'capstone-server.mysql.database.azure.com'A
+app.config['MYSQL_HOST'] = 'capstone-server.mysql.database.azure.com'
 app.config['MYSQL_CURSORCLASS'] = 'DictCursor'
 # added to use https
 # app.config['SERVER_NAME'] = 'yourdomain.com'  # replace with your domain name
